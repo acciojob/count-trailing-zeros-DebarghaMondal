b44eb107355dd46540546269c3a3a67a4d41afcc
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-08-06 09:51:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function trailingZeros(n) {
   //your JS code here. If required.
-}
+	if ( n === 0) return 0;
 
+	let numstr = n.toString();
+
+	let count = 0;
+
+	for(let i = numstr.length - 1; i >= 0; i--){
+		if(numstr[i] === '0'){
+			count++;
+		}else{
+			break
+		}
+	}
+	return count;
+}
 const input = prompt("Enter a number");
 alert(trailingZeros(input));
